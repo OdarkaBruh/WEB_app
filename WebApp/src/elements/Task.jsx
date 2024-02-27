7b7d6763cbda923bd797efb9cb81eed9c49546ab
OdarkaBruh <trentidarey@gmail.com>
2024-02-27 13:09:18 +0200

    Added time

--- WebApp/src/elements/Task.jsx
+++ WebApp/src/elements/Task.jsx
@@ -1,16 +1,33 @@
-import tasks from './Tasks.json';
+import tasks from '../data/Tasks.json';
+import './Task.css';
+
+
+//Paste image stuff here later
+function IsTaskFinished(a) {
+    if (a.finished){
+        return (<h1>+ </h1>);
+}
+return (<h1>- </h1>);
+}
+
 
 function printTasks() {
     return (
     <>
-        <div className='task'>
-            <h2>{tasks.length}</h2>
+        <div className='tasksAll'>
             {tasks.map(item => (
-                <div key={item.id}>
-                    <h1>{item.name}</h1>
-                    <p>{item.desc}</p>
-                    {item.subtask.length!==0 && printSubTasks(item.subtask)}
-                </div>
+                    <div key={item.id}>
+                        <div className='task'>
+                            <div className='taskTop'>
+                            {IsTaskFinished(item)}
+                            <h1>{item.name}   - {item.subtask.length!==0 && <a>0/{item.subtask.length}</a>}</h1>
+                            </div>
+                            <h2>{item.desc}</h2>
+                            <h1>{new Date(item.deadline).toLocaleDateString()}</h1>
+                            {item.subtask.length!==0 && printSubTasks(item.subtask)}
+                        </div>
+                    </div>
+                
             ))}
         </div>
     </>)
@@ -22,7 +39,7 @@ function printSubTasks(subtasks) {
         <div className='subTasks'>
             {subtasks.map(sub => (
                 <div key={sub.id}>
-                    <h1>{sub.name}</h1>
+                    <h3>{sub.name}</h3>
                 </div>
             ))}
         </div>
