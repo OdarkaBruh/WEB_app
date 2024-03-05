52063e4b85fda79e548e1311e4bcc5b007a85c74
OdarkaBruh <trentidarey@gmail.com>
2024-03-05 17:01:44 +0200

    more info

--- WebApp/src/App.jsx
+++ WebApp/src/App.jsx
@@ -5,7 +5,6 @@ import './App.css'
   export default function App() {
   return (
     <>
-    <h1> TEST</h1>
     <ShowTask />
     </>
   )
--- WebApp/src/elements/Task.jsx
+++ WebApp/src/elements/Task.jsx
@@ -1,4 +1,5 @@
 import tasks from '../data/Tasks.json';
+import { useState } from 'react';
 import './Task.css';
 
 
@@ -10,25 +11,81 @@ function IsTaskFinished(a) {
 return (<h1>- </h1>);
 }
 
+function dateConvector(a){
+    return new Date(a.deadline).toLocaleDateString()
+}
+
+function printFullTasks(id) {
+    let task = tasks[id];
+    return(
+    <>
+            <div className='CertainTaskTop'>
+                <h1>{IsTaskFinished(task)} {task.name}</h1>
+            </div>
+            <div className='CertainTaskInfo'>
+                <div className='DeadlineAndPriority'>
+                <h1>Until day: {dateConvector(task)}</h1>
+                <h1>Priority: {task.priority}</h1>
+                <p>{task.desc}</p>
+                </div>
+                <p>dadsasd</p>
+            </div>
+    </>)
+}
+
+function CreateFullTasks() {
+    return(
+    <>
+            <div className='CertainTaskTop'>
+                <textarea id="name" maxLength="60"></textarea>
+            </div>
+            <div className='CertainTaskInfo'>
+                <div className='DeadlineAndPriority'>
+                <h1>Until day:&emsp;<input type="date"></input></h1>
+                <br />
+                <h1>Priority:&emsp;<input type="number" max={5} min={0}></input></h1>
+                <textarea id="desc" placeholder="Describe task..." maxLength="100"></textarea>
+                </div>
+            </div>
+    </>)
+}
 
 function printTasks() {
+    const [fullTask, setFullTask] = useState(0);
+
+    
     return (
     <>
-        <div className='tasksAll'>
-            {tasks.map(item => (
-                    <div key={item.id}>
+        <div className='ViewDetailed'>
+            <div className='tasksAll'>
+                <div className='Top'>
+                    <h1>Today</h1>
+                </div>
+                <div className='Bottom'>
+                {tasks.map(item => (
+                    <div key={item.id} className={item.id===fullTask ? 'choosenFullTask' : 'v'} onClick={()=>setFullTask(item.id)}>
                         <div className='task'>
                             <div className='taskTop'>
                             {IsTaskFinished(item)}
                             <h1>{item.name}   - {item.subtask.length!==0 && <a>0/{item.subtask.length}</a>}</h1>
                             </div>
                             <h2>{item.desc}</h2>
-                            <h1>{new Date(item.deadline).toLocaleDateString()}</h1>
-                            {item.subtask.length!==0 && printSubTasks(item.subtask)}
-                        </div>
+
+                            {/*
+                            *<h2>{dateConvector(item)}</h2>
+                            {item.subtask.length!==0 && printSubTasks(item.subtask)}*/}
+                            </div>
                     </div>
-                
-            ))}
+                    
+                ))}
+            </div>
+            </div>
+            
+            <div className='CertainTask'>
+            {//{printFullTasks(fullTask)} 
+            }
+            {CreateFullTasks()}
+            </div>
         </div>
     </>)
 }
@@ -54,7 +111,6 @@ export default function ShowTask() {
     var taskDescription;
     return(
     <>
-    <h1>ЗАГОЛОВОК</h1>
     {printTasks()}
     </>
     )
